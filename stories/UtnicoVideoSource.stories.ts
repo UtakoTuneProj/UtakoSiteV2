a230a882597ce65aa46b2a835b15f91bb8be8f2f
Nanamachi <7machi@nanamachi.net>
2021-07-12 01:10:38 +0900

    fix(stories): use addon-control, not knob

--- stories/UtnicoVideoSource.stories.ts
+++ stories/UtnicoVideoSource.stories.ts
@@ -1,15 +1,15 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import UtNicoVideoSource from '@/components/atoms/UtNicoVideoSource.vue'
 
 export default {
   title: 'atoms/UtNicoVideoSource',
-  decorators: [withKnobs],
+  components: UtNicoVideoSource,
 }
+
 export const asComponent = () => ({
   components: { UtNicoVideoSource },
   props: {
     movieId: {
-      default: text('movieId', 'sm9'),
+      default: 'sm9',
     },
   },
   template: `<UtNicoVideoSource :movieId="movieId"/>`,
